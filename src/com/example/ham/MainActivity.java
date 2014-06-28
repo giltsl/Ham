46b0179c05999e547a5109979be6c50d4359ef08
Gil Tselenchuk <giltsl@mystevie.com>
2014-06-28 21:25:01 +0300

    no ui

--- src/com/example/ham/MainActivity.java
+++ src/com/example/ham/MainActivity.java
@@ -12,10 +12,12 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+//		setContentView(R.layout.activity_main);
 		
 		mPlayer = MediaPlayer.create(this, R.raw.ham);
 		mPlayer.start();
+		finish();
+		/*
 		View hamButton = findViewById(R.id.ham_button);
 		hamButton.setOnClickListener(new OnClickListener() {
 			
@@ -25,6 +27,7 @@ public class MainActivity extends Activity {
 				
 			}
 		});
+		*/
 		
 		
 		

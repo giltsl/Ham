e01dde10b1e443eb2cdf8c3233e465456dddaa70
Gil Tselenchuk <giltsl@gmail.com>
2015-02-18 13:19:18 +0200

    run jenkins/jenkins_test.sh

---
+++ jenkins/jenkins_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl -Lo jenkins.war http://mirrors.jenkins-ci.org/war/latest/jenkins.war
+ls
+java -jar jenkins.war &
+ls ~/.jenkins/war/WEB-INF/jenkins-cli.jar
+
+java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ install-plugin TestFairy.hpi
+java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ create-job JenkinsTest < JenkinsTest.xml
+java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ list-jobs
+java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ build JenkinsTest
+java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ console
+
+
+

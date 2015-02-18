1e29e3b70be0d5e3a0108a6d9d58c8077ec04967
Gil Tselenchuk <giltsl@gmail.com>
2015-02-18 17:28:52 +0200

    fix jenkins/jenkins_test.sh

--- jenkins/jenkins_test.sh
+++ jenkins/jenkins_test.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
 curl -Lo jenkins.war http://mirrors.jenkins-ci.org/war/latest/jenkins.war
-ls
-java -jar jenkins.war &
-sleep 5m
-ls ~/.jenkins/war/WEB-INF/jenkins-cli.jar
+ls;
 
+java -jar jenkins.war&
+sleep 15
+
+ls ~/.jenkins/war/WEB-INF/jenkins-cli.jar
 java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ install-plugin TestFairy.hpi
 java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ create-job JenkinsTest < JenkinsTest.xml
 java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ list-jobs
 java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ build JenkinsTest
-java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ console
 
 
+while true; do
 
+    java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ console JenkinsTest > /tmp/console.log
+    sleep 1
+
+    echo check FAILURE
+    grep "Finished: FAILURE" /tmp/console.log
+    if [[ $? == 0 ]]; then
+        echo Finished: FAILURE
+        cat /tmp/console.log
+        exit -1;
+    fi
+
+    echo check SUCCESS
+    grep "Finished: SUCCESS" /tmp/console.log
+    if [[ $? == 0 ]]; then
+        echo Yay the build work
+        exit 0;
+    fi
+
+
+done

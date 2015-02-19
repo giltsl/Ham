3956c62ec65a4aa19e6b51b683c6fcec12bdcdcc
koding user <giltsl@testfairy.com>
2015-02-19 10:06:41 +0000

    restart jenkins after install-plugin

--- jenkins/jenkins_test.sh
+++ jenkins/jenkins_test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+sudo cp zipalign /bin/
+
 curl -Lo jenkins.war http://mirrors.jenkins-ci.org/war/latest/jenkins.war
 ls;
 
 java -jar jenkins.war&
-sleep 45
+sleep 25
 
 ls ~/.jenkins/war/WEB-INF/jenkins-cli.jar
 java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ install-plugin TestFairy.hpi
+java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ restart
+sleep 15
+java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ list-plugins
 java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ create-job JenkinsTest < JenkinsTest.xml
 java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ list-jobs
 java -jar ~/.jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ build JenkinsTest
